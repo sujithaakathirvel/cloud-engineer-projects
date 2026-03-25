640241cf1dca1a1d88b9c0cab308763fdf4bff6c
sujithaakathirvel <sujithaakathirvel@gmail.com>
2026-03-25 18:19:34 +0000

    added terraform project

---
+++ terraform-remote-state-project/main.tf
@@ -0,0 +1,82 @@
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "suji-tf-state-22072003"
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
+resource "aws_security_group" "lab_sg" {
+  name        = "lab-security-group"
+  description = "Allow SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lab_security_group"
+  }
+}
+
+
+resource "aws_instance" "lab_ec2" {
+  ami           = "ami-087c9ba923d9765d8"  
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.lab_sg.id]
+
+  tags = {
+    Name = "lab-ec2-updated"
+  }
+}
